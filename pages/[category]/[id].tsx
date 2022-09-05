f66db3bc4aa8ed348cc4877933d18139b892ce7a
Stanley Ulili <stanleyulili@gmail.com>
2022-09-05 16:50:55 +0200

    add updated on feature on blog post, and modify the HTML structure

--- pages/[category]/[id].tsx
+++ pages/[category]/[id].tsx
@@ -1,4 +1,5 @@
 import Layout from "../../components/layout";
+import NextLink from "next/link";
 import { getAllPostIds, getPostData } from "../../lib/blog-posts";
 import Head from "next/head";
 import Date from "../../components/date";
@@ -27,11 +28,32 @@ export default function Post({ postData }) {
         <title>{postData.title}</title>
       </Head>
       <article>
-        <h1 className="">{postData.title}</h1>
-        <div className="">
-          <Date dateString={postData.date} />
-        </div>
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        <header>
+          <div className="wrapper">
+            <h1 className="">{postData.title}</h1>
+            <div className="post-metadata">
+              {postData.tags.map((tag) => (
+                <NextLink href={`/tags/${tag}`} key={tag}>
+                  <a>{tag}</a>
+                </NextLink>
+              ))}
+            </div>
+            <div className="post-author">
+              By Stanley Ulili on <Date dateString={postData.date} />
+              {postData.last_modified_at && (
+                <span>
+                  (Updated on <Date dateString={postData.last_modified_at} />)
+                </span>
+              )}
+            </div>
+            <p>A short description for the article</p>
+          </div>
+        </header>
+
+        <div
+          dangerouslySetInnerHTML={{ __html: postData.contentHtml }}
+          className="wrapper"
+        />
       </article>
     </Layout>
   );
